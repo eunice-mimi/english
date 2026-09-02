3ee126933a1238bd3e2935c51ad4f002ec49aecf
eunice-mimi <kimmimi2day@gmail.com>
2026-09-02 22:47:13 +0900

    Add files via upload

--- data/lessons.js
+++ data/lessons.js
@@ -1,5 +1,11 @@
 window.LESSONS = [
   {
+    date: "2026-09-02",
+    label: "09.02",
+    title: "AI, Climate Change & Daily Life",
+    file: "./lessons/2026-09-02.js"
+  },
+  {
     date: "2026-09-01",
     label: "09.01",
     title: "Typhoon, Power Outage & Animals",
